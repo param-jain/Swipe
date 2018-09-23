4ae3e6675a3a9fb2ad70799abb03c15fd59b9141
Param Jain <param_jain@icloud.com>
2018-09-23 12:41:14 +0530

    Added Swipe Gesture to the Card
    Animated a Single Card to move according to user's touch.
    Added rotation to the card according to user's change in x direction i.e. interpolated rotation to linear drag in x direction.
    Limited the rotation to -120deg to 120deg considering the user's mobile screen size by making use of Dimensions object of react-native.
    Added Spring animation to reset the position of Card on touch release.
    Forced swipe the card out of the screen if user dragged the card more than quater of the screen in the respective directions.

--- swipe/src/Deck.js
+++ swipe/src/Deck.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
 
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = Dimensions.get('window').width * 0.25;
+const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
 
@@ -13,27 +16,77 @@ class Deck extends Component {
             onPanResponderMove: (event, gesture) => {
                 position.setValue({ x: gesture.dx, y: gesture.dy })
             },
-            onPanResponderRelease: () => {}
+            onPanResponderRelease: (event, gesture) => {
+                if (gesture.dx > SWIPE_THRESHOLD) {
+                    this.forceSwipe('right');
+                } else if (gesture.dx < -SWIPE_THRESHOLD) {
+                    this.forceSwipe('left');
+                } else {
+                    this.resetPosition();
+                }
+            }
         });
 
         this.position = position;
         this.state = { panResponder, position };
     }
 
+    resetPosition() {
+        Animated.spring(this.state.position, {
+            toValue: { x:0, y:0 }
+        }).start();
+    }
+
+    forceSwipe(direction) {
+        const x = direction === 'right' ? SCREEN_WIDTH : -SCREEN_WIDTH;
+        Animated.timing(this.state.position, {
+           toValue: { x, y: 0 },
+           duration: SWIPE_OUT_DURATION
+        }).start(() => this.onSwipeComplete(direction));
+    }
+
+    onSwipeComplete(direction) {
+        const { onSwipeLeft, onSwipeRight } = this.props;
+
+        direction === 'right' ? onSwipeRight() : onSwipeLeft();
+    }
+
+    getCardStyle() {
+
+        const { position } = this.state;
+        const rotate = position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH*1.5, 0, SCREEN_WIDTH*1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+
+        return {
+            ...position.getLayout(),
+            transform: [{ rotate }]
+        };
+    };
+
     renderCards() {
-        return this.props.data.map(item => {
+        return this.props.data.map((item, index) => {
+            if (index === 0) {
+                return (
+                    <Animated.View
+                        key={item.id}
+                        style={this.getCardStyle()}
+                        {...this.state.panResponder.panHandlers}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                );
+            }
             return this.props.renderCard(item);
         });
     }
 
     render() {
         return (
-            <Animated.View 
-                style={this.state.position.getLayout()}
-                {...this.state.panResponder.panHandlers}
-            >
+            <View>
                 {this.renderCards()}
-            </Animated.View>
+            </View>
         );
     }
 }
